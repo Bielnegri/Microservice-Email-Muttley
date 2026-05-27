78051773cc9c1e9c7080cda7f03ddb3ab562e2fa
Bielnegri <gabrielbnegri@gmail.com>
2026-05-27 15:47:53 -0300

    :bug: fix: Corrige algumas configurações para comunicação entre serviços

--- src/main/java/com/ms/email/email/EmailConsumer.java
+++ src/main/java/com/ms/email/email/EmailConsumer.java
@@ -1,11 +1,13 @@
 package com.ms.email.email;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ms.email.email.dto.EventoEmail;
 import com.ms.email.email.dto.InscricaoEmail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 @RequiredArgsConstructor
@@ -14,20 +16,30 @@ public class EmailConsumer {
 
     private final EmailService emailService;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @KafkaListener(topics = "email.inscricao.confirmada", groupId = "email-group")
-    public void consumirConfirmacao(InscricaoEmail dto) {
+    public void consumirConfirmacao(String payload) throws JsonProcessingException {
+        InscricaoEmail dto = objectMapper.readValue(payload, InscricaoEmail.class);
+
         log.info("Processando confirmação: {}", dto.destinatario());
         emailService.enviarConfirmacaoCadastro(dto);
+
     }
 
     @KafkaListener(topics = "email.evento.cancelado", groupId = "email-group")
-    public void consumirCancelado(EventoEmail dto) {
+    public void consumirCancelado(String payload) throws JsonProcessingException {
+
+        EventoEmail dto = objectMapper.readValue(payload, EventoEmail.class);
+
         log.info("Processando cancelamento: {}", dto.destinatario());
         emailService.enviarEventoCancelado(dto);
     }
 
     @KafkaListener(topics = "email.evento.concluido", groupId = "email-group")
-    public void consumirConcluido(EventoEmail dto) {
+    public void consumirConcluido(String payload) throws JsonProcessingException {
+        EventoEmail dto = objectMapper.readValue(payload, EventoEmail.class);
+
         log.info("Processando conclusão: {}", dto.destinatario());
         emailService.enviarEventoConcluido(dto);
     }
